c36474e944ef8b757f572a00171c4f76f2d3e2fb
Rigidus <i.am.rigidus@gmail.com>
2023-03-22 07:57:40 +0300

    fix path

--- library.lisp
+++ library.lisp
@@ -7,19 +7,23 @@
 
 (cffi:use-foreign-library tesseract)
 
-(defparameter *tessdata-directory* 
+(defparameter *tessdata-directory*
   #+unix
   (namestring (or (probe-file "/usr/local/share/tessdata") ; Homebrew
-                  (probe-file "/usr/local/tessdata")))
+                  (probe-file "/usr/local/tessdata")
+                  (probe-file "/usr/share/tesseract-ocr/4.00/tessdata")))
   #+windows
   (namestring (probe-file "C:\\Program Files\\Tesseract OCR\\tessdata"))
-  "*tessdata-directory* should point to the location of the directory containing 
-.traineddata files for use by Tesseract. The value must be a string representing the full 
-path to the directory. 
+  "*tessdata-directory* should point to the location of the directory containing
+.traineddata files for use by Tesseract. The value must be a string representing the full
+path to the directory.
 
-This searches in a common default location for each platform and will follow symlinks to 
+This searches in a common default location for each platform and will follow symlinks to
 find the true location of the tessdata directory if one exists.
 
 If your .traineddata files are in a non-standard location, it can be shadowed; i.e.
 (let ((*tessdata-directory* \"/path/to/tessdata\"))
      (image-to-text #P\"~/eurotext.jpg\"))")
+
+;; /usr/lib/x86_64-linux-gnu/libtesseract.so
+;; /usr/share/tesseract-ocr/4.00/tessdata
